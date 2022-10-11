984041252349da5466546f10792591b6e51449f3
Tanvir Ahmed <tanvir.be.global@gmail.com>
2022-10-11 14:02:29 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Home from './pages/Home/Home';
+import Navbar from './SharedComponent/Navbar/Navbar';
 
 function App() {
   return (
-    <div>
-      <h1 className='text-red-700'>Hello World</h1>
-    </div>
+    <div className='bg-gray-100'>
+      <div className='px-2 min-h-screen'>
+        <Navbar />
+        <Home />
+      </div></div>
   );
 }
 
